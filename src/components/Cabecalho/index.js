6fd3ee88af8e0b0085ad5679645f9b8d08cfa146
Matheus Alves <matheus.alvsbarroso@gmail.com>
2024-03-03 20:22:25 -0300

    aula 1 Primeiros Passos

---
+++ src/components/Cabecalho/index.js
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+import logo from "./Logo.png";
+import styles from "./Cabecalho.module.css";
+import CabecalhoLink from "components/CabecalhoLink";
+
+function Cabecalho() {
+  return (
+    <header className={styles.cabecalho}>
+      <Link to="./">
+        <img src={logo} alt="Logo do Ccinetag"></img>
+      </Link>
+      <nav>
+        <CabecalhoLink url="./">Home</CabecalhoLink>
+        <CabecalhoLink url="./Favoritos">Favoritos</CabecalhoLink>
+      </nav>
+    </header>
+  );
+}
+
+export default Cabecalho;
---
+++ src/components/Rodape/index.js
@@ -0,0 +1,13 @@
+import styles from "./Rodape.module.css"
+
+
+function Rodape(){
+    return(
+        <footer className={styles.rodape}>
+            <h2>Desenvolvido por Matheus Estudante Alura</h2>
+        </footer>
+    )
+
+}
+
+export default Rodape;
